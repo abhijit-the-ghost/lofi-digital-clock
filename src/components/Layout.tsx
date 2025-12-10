cf5116330ef5efedda1fdf787719b6678d0dbf03
abhijit-the-ghost <nepweebs@gmail.com>
2025-12-10 20:38:44 +0545

    Feature: created favicon and changed the name

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -44,6 +44,19 @@ const Layout = ({ left, right, theme, themeSelector }: LayoutProps) => {
             <div className="absolute bottom-6 right-6 z-50">
                 {themeSelector}
             </div>
+
+            {/* GitHub Link (Bottom Center) */}
+            <div className="absolute bottom-4 left-1/2 -translate-x-1/2 z-50">
+                <a
+                    href="https://github.com/abhijit-the-ghost/lofi-digital-clock"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-xs font-[VT323] opacity-40 hover:opacity-100 transition-opacity tracking-widest uppercase"
+                    style={{ color: theme.colors.text }}
+                >
+                    View on GitHub
+                </a>
+            </div>
         </div>
     );
 };
